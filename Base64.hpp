3d709a3c57adfbb4bb1505a30e2f820215c03517
chenjunfu <64954184+chenjunfu2@users.noreply.github.com>
2023-01-04 10:50:37 +0800

    Add files via upload

--- Base64.hpp
+++ Base64.hpp
@@ -1,250 +1,303 @@
-#pragma once
+#ifndef Base64_hpp
+#define Base64_hpp
 
 //***************define***************//
-#define NULL 0
+#define DEFAULT_BASECODE "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"//MIME Base64
+#define DEFAULT_FULLCODE '='//MIME Base64
+#define INIT_THROW true//抛出初始化异常
+
 #define CONST const
+#define NOEXCEPT noexcept
+
+#define BASECODE_COUNT ((ULONG)(64))//BASE64
+#define BASECODEAP_COUNT (((ULONG)((UCHAR)(-1)))+1)//UCHAR_MAX+1
+
+#if INIT_THROW
+#define THROW_ERROR(reason) throw(reason)
+#else
+#define THROW_ERROR(reason)
+#endif
 //***************define***************//
+
+
+//***************include***************//
+#include <string.h>
+//***************include***************//
+
 
 //***************class***************//
 class Base64
 {
 public:
+	typedef char CHAR;
+	typedef char *PCHAR;
+	typedef const char CCHAR;
+	typedef const char *PCCHAR;
 	typedef unsigned char UCHAR;
-	typedef unsigned char* PUCHAR;
+	typedef unsigned char *PUCHAR;
 	typedef const unsigned char CUCHAR;
-	typedef const unsigned char* PCUCHAR;
+	typedef const unsigned char *PCUCHAR;
 	typedef long LONG;
 	typedef unsigned long ULONG;
 	typedef unsigned long long ULONGLONG;
 	typedef void VOID;
-	typedef void* PVOID;
-	typedef const void* PCVOID;
+	typedef void *PVOID;
+	typedef const void *PCVOID;
 	typedef bool BOOL;
 protected:
-	UCHAR FullCode;//填充字符
-	UCHAR BaseText[65];//加密字符集 Use:[0]~[64] Full:[64]=0 Text:[0]~[63]
-	UCHAR BaseTextMap[256];//加密字符集映射：通过加密字符获得该字符在字符集中的位置（映射集）
+	UCHAR ucFullCode;//填充字符
+	UCHAR ucBaseCode[BASECODE_COUNT + sizeof('\0')];//加密字符集 Use:[0]~[64] Full:[64]=0 Text:[0]~[63]
+	UCHAR ucBaseCodeMap[BASECODEAP_COUNT];//加密字符集映射：通过加密字符获得该字符在字符集中的位置（映射集）
+	BOOL bAvailable;//类是否可用（如果ucBaseCode内数据不正确、映射不成功则该值为false，类不可用，否则可用），用于保证加密数据不因加密串问题而受到损坏
+
+	//字符集映射函数
+	BOOL MapTheBaseCode(VOID) NOEXCEPT;
 public:
-	Base64(VOID);
-	Base64(UCHAR, PCUCHAR);
-	Base64(PCUCHAR);
-	Base64(UCHAR);
+	Base64(PCUCHAR _ucBaseCode = (PCUCHAR)DEFAULT_BASECODE, UCHAR _ucFullCode = DEFAULT_FULLCODE);//构造类
+	Base64(CONST Base64 &) = default;
+	Base64(Base64 &&) = default;
 	~Base64(VOID) = default;
 
-	VOID SetBaseText(PCUCHAR);//设置加密字符串
-	PCUCHAR GetBaseText(VOID) CONST;//获取加密字符串
+	BOOL SetBaseCode(PCUCHAR pcucBaseCode) NOEXCEPT;//设置加密字符串
+	PCUCHAR GetBaseCode(VOID) CONST NOEXCEPT;//获取加密字符串
 
-	VOID SetFullCode(UCHAR);//设置填充字符
-	UCHAR GetFullCode(VOID) CONST;//获取填充字符
+	BOOL SetFullCode(UCHAR cucFullCode) NOEXCEPT;//设置填充字符
+	UCHAR GetFullCode(VOID) CONST NOEXCEPT;//获取填充字符
 
-	PCUCHAR GetBaseTextMap(VOID) CONST;//获取映射表
+	ULONGLONG GetEnCodeSize(PCVOID pcEnCodeData, ULONGLONG ullEncodeSize) CONST NOEXCEPT;//获取加密后字符串长度
+	BOOL EnCode(PCVOID pcData, ULONGLONG ullDataSize, PUCHAR pEncode) CONST NOEXCEPT;//加密指定内存的指定字节数到指定字符串
 
-	ULONGLONG GetEnCodeSize(PCVOID, ULONGLONG) CONST;//获取加密后字符串长度
-	VOID EnCode(PCVOID, ULONGLONG, PUCHAR) CONST;//加密指定内存的指定字节数到指定字符串
-
-	ULONGLONG GetDeCodeSize(PCUCHAR, ULONGLONG) CONST;//获取解密后数据字节长度
-	BOOL DeCode(PCUCHAR, ULONGLONG, PVOID) CONST;//解密指定字符串到指定内存的指定字节数
+	ULONGLONG GetDeCodeSize(PCUCHAR pcDeCodeText, ULONGLONG ullDecodeSize) CONST NOEXCEPT;//获取解密后数据字节长度
+	BOOL DeCode(PCUCHAR pcucCode, ULONGLONG ullCodeSize, PVOID pData) CONST NOEXCEPT;//解密指定字符串到指定内存的指定字节数
 };
 //***************class***************//
 
 //***************funtion***************//
-Base64::Base64(VOID) :
-	FullCode('='),
-	BaseText("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/")
+
+Base64::BOOL Base64::MapTheBaseCode(VOID) NOEXCEPT
 {
-	for (ULONG i = 0; i < 256; i += 1)
-		BaseTextMap[i] = 64;//全部初始化为64
+	//将ucBaseCodeMap初始化为BASECODE_COUNT,以便在后续检查中发现错误
+	memset(ucBaseCodeMap, BASECODE_COUNT, BASECODEAP_COUNT * sizeof(UCHAR));
 
-	for (ULONG i = 0; i < 64; i += 1)
-		BaseTextMap[BaseText[i]] = (UCHAR)i;//创建字符集映射
-
-	return;
-}
-
-Base64::Base64(UCHAR F, PCUCHAR B) :
-	FullCode(F)
-{
-	for (ULONG i = 0; i < 256; i += 1)
-		BaseTextMap[i] = 64;//全部初始化为64
-
-	for (ULONG i = 0; i < 64; i += 1)
+	for (ULONG i = 0; i < BASECODE_COUNT; i += 1)
 	{
-		BaseText[i] = B[i];//拷贝传入字符集
-		BaseTextMap[BaseText[i]] = (UCHAR)i;//同时映射字符集
+		if (ucBaseCodeMap[ucBaseCode[i]] != BASECODE_COUNT)
+		{
+			return false;//重复字符
+		}
+		ucBaseCodeMap[ucBaseCode[i]] = (UCHAR)i;//映射加密字符(快速解码用)
 	}
-	BaseText[64] = '\0';
 
-	return;
+	return true;
 }
 
-Base64::Base64(UCHAR F) :
-	FullCode(F),
-	BaseText("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/")
+Base64::Base64(PCUCHAR _ucBaseCode, UCHAR _ucFullCode) :
+	ucFullCode(_ucFullCode), bAvailable(false)
 {
-	for (ULONG i = 0; i < 256; i += 1)
-		BaseTextMap[i] = 64;//全部初始化为64
+	//拷贝输入字符串
+	memcpy(ucBaseCode, _ucBaseCode, BASECODE_COUNT);
+	ucBaseCode[64] = '\0';//安全起见给字符串末尾赋值为0
 
-	for (ULONG i = 0; i < 64; i += 1)
-		BaseTextMap[BaseText[i]] = (UCHAR)i;//创建字符集映射
-
-	return;
-}
-
-Base64::Base64(PCUCHAR B) :
-	FullCode('=')
-{
-	for (ULONG i = 0; i < 256; i += 1)
-		BaseTextMap[i] = 64;//全部初始化为64
-
-	for (ULONG i = 0; i < 64; i += 1)
+	//检查填充字符是否位于加密串中
+	if (strchr((PCCHAR)ucBaseCode, ucFullCode) != NULL)
 	{
-		BaseText[i] = B[i];//拷贝传入字符集
-		BaseTextMap[BaseText[i]] = (UCHAR)i;//同时映射字符集
+		THROW_ERROR("加密串中出现填充字符!");//填充字符与字符串中的某个字符相同
+		return;
 	}
-	BaseText[64] = '\0';
 
-	return;
-}
-
-
-Base64::VOID Base64::SetBaseText(PCUCHAR pcBaseText)
-{
-	for (ULONG i = 0; i < 64; i += 1)
-		BaseTextMap[BaseText[i]] = 64;//读取映射字符集并重新归位（避免全部初始化，减少性能开销）
-
-	for (ULONG i = 0; i < 64; i += 1)
+	//映射加密串
+	if (!MapTheBaseCode())
 	{
-		BaseText[i] = pcBaseText[i];//设置字符集
-		BaseTextMap[BaseText[i]] = (UCHAR)i;//同时映射字符集
+		THROW_ERROR("加密串中出现重复字符!");
+		return;
 	}
-	BaseText[64] = '\0';
 
-	return;
+	//成功初始化，该类可用
+	bAvailable = true;
 }
 
-Base64::PCUCHAR Base64::GetBaseText(VOID) CONST
+
+Base64::BOOL Base64::SetBaseCode(PCUCHAR pcucBaseCode) NOEXCEPT
 {
-	return BaseText;
+	//设置该类为不可用状态
+	bAvailable = false;
+
+	//拷贝输入字符串
+	memcpy(ucBaseCode, pcucBaseCode, BASECODE_COUNT);
+	ucBaseCode[64] = '\0';//安全起见给字符串末尾赋值为0
+
+	//映射加密串
+	bAvailable = MapTheBaseCode();
+	return bAvailable;
 }
 
-Base64::VOID Base64::SetFullCode(UCHAR cFullCode)
+Base64::PCUCHAR Base64::GetBaseCode(VOID) CONST NOEXCEPT
 {
-	FullCode = cFullCode;
+	return ucBaseCode;
 }
 
-Base64::UCHAR Base64::GetFullCode(VOID) CONST
+
+Base64::BOOL Base64::SetFullCode(UCHAR cucFullCode) NOEXCEPT
 {
-	return FullCode;
+	//设置该类为不可用状态
+	bAvailable = false;
+	//设置填充字符
+	ucFullCode = cucFullCode;
+
+	//验证字符
+	bAvailable = strchr((PCCHAR)ucBaseCode, ucFullCode) == NULL;//返回NULL代表填充字符不在加密串中
+	return bAvailable;
 }
 
-Base64::PCUCHAR Base64::GetBaseTextMap(VOID) CONST
+Base64::UCHAR Base64::GetFullCode(VOID) CONST NOEXCEPT
 {
-	return BaseTextMap;
+	return ucFullCode;
 }
 
-Base64::ULONGLONG Base64::GetEnCodeSize(PCVOID pcEnCodeData, ULONGLONG ullEncodeSize) CONST
+
+Base64::ULONGLONG Base64::GetEnCodeSize(PCVOID pcEnCodeData, ULONGLONG ullEncodeSize) CONST NOEXCEPT
 {
-	return ullEncodeSize <= 3 ? 4: (ullEncodeSize / 3 * 4 + ((ullEncodeSize % 3) ? 4 : 0));
+	return ullEncodeSize <= 3 ? 4 : (ullEncodeSize / 3 * 4 + ((ullEncodeSize % 3) ? 4 : 0));
 }
 
-Base64::VOID Base64::EnCode(PCVOID pcData, ULONGLONG size, PUCHAR pEncode) CONST
+Base64::BOOL Base64::EnCode(PCVOID pcData, ULONGLONG ullDataSize, PUCHAR pEncode) CONST NOEXCEPT
 {
-	PCUCHAR pcCode = (PCUCHAR)pcData;
+	//类的状态为不可用
+	if (!bAvailable)
+	{
+		return false;
+	}
+
+	PCUCHAR pcucCode = (PCUCHAR)pcData;
 	ULONGLONG i, j;
 
-	for (i = 3, j = 4; i <= size; i += 3, j += 4)
+	for (i = 3, j = 4; i <= ullDataSize; i += 3, j += 4)
 	{
-		pEncode[j - 4] = BaseText[(pcCode[i - 3] >> 2)];
-		pEncode[j - 3] = BaseText[(((pcCode[i - 3] & 0x03) << 4) | ((pcCode[i - 2] & 0xF0) >> 4))];
-		pEncode[j - 2] = BaseText[(((pcCode[i - 2] & 0x0F) << 2) | ((pcCode[i - 1] & 0xC0) >> 6))];
-		pEncode[j - 1] = BaseText[pcCode[i - 1] & 0x3F];
+		//加密3字节数据到4个Base64字符
+		pEncode[j - 4] = ucBaseCode[(pcucCode[i - 3] >> 2)];
+		pEncode[j - 3] = ucBaseCode[(((pcucCode[i - 3] & 0x03) << 4) | ((pcucCode[i - 2] & 0xF0) >> 4))];
+		pEncode[j - 2] = ucBaseCode[(((pcucCode[i - 2] & 0x0F) << 2) | ((pcucCode[i - 1] & 0xC0) >> 6))];
+		pEncode[j - 1] = ucBaseCode[pcucCode[i - 1] & 0x3F];
 	}
 
-	switch ((size % 3) - 1)
+	switch ((ullDataSize % 3) - 1)
 	{
-	case 0:
-		pEncode[j - 4] = BaseText[(pcCode[i - 3] >> 2)];
-		pEncode[j - 3] = BaseText[((pcCode[i - 3] & 0x03) << 4)];
-		pEncode[j - 2] = FullCode;
-		pEncode[j - 1] = FullCode;
-		break;
-	case 1:
-		pEncode[j - 4] = BaseText[(pcCode[i - 3] >> 2)];
-		pEncode[j - 3] = BaseText[(((pcCode[i - 3] & 0x03) << 4) | ((pcCode[i - 2] & 0xF0) >> 4))];
-		pEncode[j - 2] = BaseText[(((pcCode[i - 2] & 0x0F) << 2) | ((pcCode[i - 1] & 0xC0) >> 6))];
-		pEncode[j - 1] = FullCode;
-		break;
-	default:
-		break;
+		case 0:
+			//加密剩余字符并设置填充字符
+			pEncode[j - 4] = ucBaseCode[(pcucCode[i - 3] >> 2)];
+			pEncode[j - 3] = ucBaseCode[((pcucCode[i - 3] & 0x03) << 4)];
+			pEncode[j - 2] = ucFullCode;
+			pEncode[j - 1] = ucFullCode;
+			break;
+		case 1:
+			//加密剩余字符并设置填充字符
+			pEncode[j - 4] = ucBaseCode[(pcucCode[i - 3] >> 2)];
+			pEncode[j - 3] = ucBaseCode[(((pcucCode[i - 3] & 0x03) << 4) | ((pcucCode[i - 2] & 0xF0) >> 4))];
+			pEncode[j - 2] = ucBaseCode[(((pcucCode[i - 2] & 0x0F) << 2) | ((pcucCode[i - 1] & 0xC0) >> 6))];
+			pEncode[j - 1] = ucFullCode;
+			break;
+		default:
+			break;
 	}
 
-	return;
+	return true;
 }
 
-Base64::ULONGLONG Base64::GetDeCodeSize(PCUCHAR pcDeCodeText, ULONGLONG ullDecodeSize) CONST
+
+Base64::ULONGLONG Base64::GetDeCodeSize(PCUCHAR pcDeCodeText, ULONGLONG ullDecodeSize) CONST NOEXCEPT
 {
-	ULONGLONG fs = 0;
+	ULONGLONG ullFullCodeCount = 0;
 	if (pcDeCodeText)
 	{
-		if (pcDeCodeText[ullDecodeSize - 2] == FullCode)
-			fs = 2;
-		else if (pcDeCodeText[ullDecodeSize - 1] == FullCode)
-			fs = 1;
+		if (pcDeCodeText[ullDecodeSize - 2] == ucFullCode)
+		{
+			ullFullCodeCount = 2;
+		}
+		else if (pcDeCodeText[ullDecodeSize - 1] == ucFullCode)
+		{
+			ullFullCodeCount = 1;
+		}
 	}
 
-	return (ullDecodeSize <= 4 ? 3 : ullDecodeSize / 4 * 3) - fs;
+	return (ullDecodeSize <= 4 ? 3 : ullDecodeSize / 4 * 3) - ullFullCodeCount;
 }
 
-Base64::BOOL Base64::DeCode(PCUCHAR pcCode, ULONGLONG size, PVOID pData) CONST
+Base64::BOOL Base64::DeCode(PCUCHAR pcucCode, ULONGLONG ullCodeSize, PVOID pData) CONST NOEXCEPT
 {
-	PUCHAR cpDecode = (PUCHAR)pData;
-	ULONGLONG i, j;
-	UCHAR cCode[4] = { 0 };
-
-	if (size % 4 != 0)
-		return false;
-
-	if (pcCode[size - 2] == FullCode)
-		size -= 3;
-	else if (pcCode[size - 1] == FullCode)
-			size -= 2;
-
-	for (i = 4, j = 3; i <= size; i += 4, j += 3)
+	//类的状态为不可用
+	if (!bAvailable)
 	{
-		for (LONG k = -4; k < 0; k += 1)
-		{
-			if ((cCode[k + 4] = BaseTextMap[pcCode[i + k]]) == 64)
-				return false;
-		}
-
-		cpDecode[j - 3] = (cCode[0] << 2) | ((cCode[1] & 0x30) >> 4);
-		cpDecode[j - 2] = (((cCode[1] & 0x0F) << 4) | ((cCode[2] & 0x3C) >> 2));
-		cpDecode[j - 1] = ((cCode[2] & 0x03) << 6) | (cCode[3] >> 0);
+		return false;
 	}
 
-	switch ((size % 4) - 1)
+	//不是4的倍数
+	if (ullCodeSize % 4 != 0)
 	{
-	case 0:
-		for (LONG k = -4; k < -2; k += 1)
+		return false;
+	}
+
+	//计算实际要解码的数据（排除填充字符）
+	if (pcucCode[ullCodeSize - 2] == ucFullCode)
+	{
+		ullCodeSize -= 3;
+	}
+	else if (pcucCode[ullCodeSize - 1] == ucFullCode)
+	{
+		ullCodeSize -= 2;
+	}
+
+	PUCHAR cpDecode = (PUCHAR)pData;
+	ULONGLONG i, j;
+	UCHAR ucCode[4] = {0};
+
+	for (i = 4, j = 3; i <= ullCodeSize; i += 4, j += 3)
+	{
+		//查找映射并检查是否存在于映射集中
+		for (LONG k = -4; k < 0; k += 1)
 		{
-			if ((cCode[k + 4] = BaseTextMap[pcCode[i + k]]) == 64)
+			if ((ucCode[k + 4] = ucBaseCodeMap[pcucCode[i + k]]) == BASECODE_COUNT)//该字符不在映射集中
+			{
 				return false;
-		}
-		
-		cpDecode[j - 3] = (cCode[0] << 2) | ((cCode[1] & 0x30) >> 4);
-		break;
-	case 1:
-		for (LONG k = -4; k < -1; k += 1)
-		{
-			if ((cCode[k + 4] = BaseTextMap[pcCode[i + k]]) == 64)
-				return false;
+			}
 		}
 
-		cpDecode[j - 3] = (cCode[0] << 2) | ((cCode[1] & 0x30) >> 4);
-		cpDecode[j - 2] = (((cCode[1] & 0x0F) << 4) | ((cCode[2] & 0x3C) >> 2));
-		break;
-	default:
-		break;
+		//解密4个Base64字符到3个字节数据
+		cpDecode[j - 3] = (ucCode[0] << 2) | ((ucCode[1] & 0x30) >> 4);
+		cpDecode[j - 2] = (((ucCode[1] & 0x0F) << 4) | ((ucCode[2] & 0x3C) >> 2));
+		cpDecode[j - 1] = ((ucCode[2] & 0x03) << 6) | (ucCode[3] >> 0);
+	}
+
+	switch ((ullCodeSize % 4) - 1)
+	{
+		case 0:
+			//查找映射并检查是否存在于映射集中
+			for (LONG k = -4; k < -2; k += 1)
+			{
+				if ((ucCode[k + 4] = ucBaseCodeMap[pcucCode[i + k]]) == BASECODE_COUNT)//该字符不在映射集中
+				{
+					return false;
+				}
+			}
+
+			//解密剩余字符
+			cpDecode[j - 3] = (ucCode[0] << 2) | ((ucCode[1] & 0x30) >> 4);
+			break;
+		case 1:
+			//查找映射并检查是否存在于映射集中
+			for (LONG k = -4; k < -1; k += 1)
+			{
+				if ((ucCode[k + 4] = ucBaseCodeMap[pcucCode[i + k]]) == BASECODE_COUNT)//该字符不在映射集中
+				{
+					return false;
+				}
+			}
+
+			//解密剩余字符
+			cpDecode[j - 3] = (ucCode[0] << 2) | ((ucCode[1] & 0x30) >> 4);
+			cpDecode[j - 2] = (((ucCode[1] & 0x0F) << 4) | ((ucCode[2] & 0x3C) >> 2));
+			break;
+		default:
+			break;
 	}
 
 	return true;
@@ -252,67 +305,8 @@ Base64::BOOL Base64::DeCode(PCUCHAR pcCode, ULONGLONG size, PVOID pData) CONST
 //***************funtion***************//
 
 //***************undef***************//
-#undef NULL
 #undef CONST
+#undef NOEXCEPT
 //***************undef***************//
 
-
-
-//***************test***************//
-/*
-	Base64 be64;
-
-	char trye[] = "你好，世界！";//xOO6w6OsysC956Oh
-
-	char* be = new char[be64.GetEnCodeSize(trye, strlen(trye)) + 1]{ 0 };
-	be64.EnCode(trye, strlen(trye), be);
-	printf("%s", be);
-
-	putchar('\n');
-
-	char tryd[] = "xOO6w6OsysC956Oh";//你好，世界！
-	Base64 bd64;
-
-	char* bd = new char[bd64.GetDeCodeSize(tryd, strlen(tryd)) + 1]{ 0 };
-	if (bd64.DeCode(tryd, strlen(tryd), bd))
-		printf("%s", bd);
-	else
-		printf("出错了");
-
-	putchar('\n');
-
-
-	delete[] be;
-	delete[] bd;
-
-	system("pause");
-*/
-/*
-	string IO;
-	Base64 b64((const unsigned char*)"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrs.uvwxy,0123456789+/");	
-
-	b64.SetBaseText("9876543210ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz+/");
-
-
-	printf("请输入你要加密的字符串:");
-	cin >> IO;
-	
-	char* be = new char[b64.GetEnCodeSize(NULL, IO.size()) + 1];
-	b64.EnCode((unsigned char*)IO.c_str(), (Base64::ULONGLONG)IO.size(), (unsigned char*)be);
-	printf("%s\n", be);
-	delete[] be;
-	
-
-
-	printf("请输入你要解密的字符串:");
-	cin >> IO;
-	
-	char* bd = new char[b64.GetDeCodeSize((unsigned char*)IO.c_str(), IO.size()) + 1];
-	b64.DeCode((unsigned char*)IO.c_str(), (Base64::ULONGLONG)IO.size(), (unsigned char*)bd);
-	printf("%s\n", bd);
-	delete[] bd;
-	
-	system("pause");
-*/
-
-//***************test***************//
+#endif // !Base64_hpp
